fbadd8d795f070149c9ebfee1cebe4c61b966065
Jam19th <jam19th@gmail.com>
2023-09-06 17:42:41 -0700

    removed product card

--- flea_market/src/components/Product/ProductWindow.js
+++ flea_market/src/components/Product/ProductWindow.js
@@ -1,19 +1,26 @@
 //Dependencies
-import ProductCard from "./ProductCard";
+import { useEffect, useState } from 'react'
+
 import "./Products.css";
 
 export default function ProductWindow({ seeds }) {
-    // const display = props.map((item,index) => {
-    //     return (
-    //         <div>
-    //         <ProductCard item={item} key={index} />
-    //         </div>
-    //     )
-    // })
+    const [products, setProducts] = useState([])
+
+    // Fetch products from API
+    useEffect(() => {
+        const FleaMarketAPI = () => {
+            const fetchData = async () => {
+                const res = await fetch('http://localhost:3000/products')
+                const json = await res.json()
+                setProducts(json)
+            }
+            fetchData()
+        }
+        FleaMarketAPI()
+    }, [])
 
     return (
         <div>
-            {/* {display} */}
             {seeds.map((seed) => {
                 const { id, name, price, image } = seed;
                 return (
@@ -30,9 +37,17 @@ export default function ProductWindow({ seeds }) {
     )
 }
 
-{/* <Routes>
-    <Route path="/" element={<ProductCard />} />
-    <Route path="/" element={<ProductCard />} />
-    <Route path="/" element={<ProductCard />} />
-    <Route path="/" element={<ProductCard />} />
-</Routes> */}
+{/* <div>
+{seeds.map((seed) => {
+    const { id, name, price, image } = seed;
+    return (
+        <article key={id}>
+            <img src={image} alt={name} />
+            <div>
+                <h4>{name}</h4>
+                <p>{price}</p>
+            </div>
+        </article>
+    )
+})}
+</div> */}
