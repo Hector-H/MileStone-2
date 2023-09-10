b9657fa4e199fbe5bdfb4ce313849cad013cd28a
Alex Hampton <meghanahampton@gmail.com>
2023-09-10 06:05:32 -0700

    pulled from main branch
    copied code from main, unable to do proper pull

--- flea_market/src/App.js
+++ flea_market/src/App.js
@@ -25,6 +25,7 @@ import ProductById from './components/Product/ProductById.js'
 import ProfileCreate from './components/Profile/ProfileCreate.js';
 import SearchResults from './components/NavBar/SearchResults.js';
 import ProductCard from './components/Product/ProductCard.js';
+import SearchResults from './components/NavBar/SearchResults.js';
 
 
 
