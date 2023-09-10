1bf292389ea549500fa7dda8ca4b3a154e52a622
Hector <h.hernandez12090@gmail.com>
2023-09-10 11:16:26 -0700

    edit and delete button

--- flea_market/src/components/Product/ProductbyId.js
+++ flea_market/src/components/Product/ProductbyId.js
@@ -4,6 +4,22 @@ import supabase from "../../supabaseClient";
 
 export default function ProductById({match}) {
     const productId = useParams();
+    const handleDelete = async () => {
+        const { data, error } = await supabase
+        .from('products')
+        .delete()
+        .eq('id', products.id)
+        .select()
+
+        if(error) { 
+            console.log(error)
+        }
+        if(data) {
+            console.log(data)
+            .onDelete(products.id)
+        }
+    }
+
 
     const [products, setProducts] = useState(null);
 
@@ -25,22 +41,24 @@ export default function ProductById({match}) {
         fetchProducts();
     })
 
-    return (
-        <div>
-            <h2>Product by Id</h2>
-            {products && (
-                <div>
-                    <h3>{products.title}</h3>
+
+return (
+    <div>
+                    <Link to={`/products/${products.id}`}>{products.title}</Link>
                     <h3>{products.details}</h3>
                     <h3>{products.dateposted}</h3>
                     <h3>${products.price}</h3>
                     <h3>{products.category}</h3>
                     <h3>{products.reviews}</h3>
                     <img src={products.image} alt="product" />
-                    <button>{products.checkOutButton}</button>
-                </div>
-            )}
-            <Link to="/">Back to Products</Link>
-        </div>
-    );
+                    {/* <button> */}
+                    <div>
+                        <Link to={'/products/edit/' + products.id}>
+                            <i className="material-icons">edit</i>
+                        </Link>
+                        <i className="material-icons" onClick={handleDelete}>delete</i>
+                        {/* {product.checkOutButton}</button> */}</div>
+                    
+    </div>
+)
 }
---
+++ flea_market/src/components/Profile/ProfileWindow.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import supabase from '../../supabaseClient'
+import ProfileCard from './ProfileCard'
+
+
+export default function ProfilePage({ profile }) {
+    const [fetchError, setFetchError] = useState(null)
+    const [profiles, setProfiles] = useState(null)
+    // const [orderBy, setOrderBy] = useState('created_at')
+
+    const handleDelete = (id) => {
+        setProfiles(prevProfiles => {
+            return prevProfiles.filter(pf => pf.id !== id)
+        })
+    }
+
+    // Fetch Profiles from API
+    useEffect(() => {
+        const fetchProfiles = async () => {
+            const { data, error } = await supabase
+                .from('profiles')
+                .select()
+                // .order(orderBy, {ascending:false})
+
+            if (error) {
+                setFetchError('could not fetch Profiles')
+                setProfiles(null)
+                console.log(error)
+            }
+            if (data) {
+                setProfiles(data)
+                setFetchError(null)
+            }
+        }
+        fetchProfiles()
+
+
+    }, [])
+
+    return (
+        <div>
+            {fetchError && (<p>{fetchError}</p>)}
+            {profiles && (
+                <div className='Profiles'>
+                    
+                    {profiles.map(profile => (
+                        <>
+                            <ProfileCard key={profile.id} profile={profile} onDelete={handleDelete}/>
+                        </>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
